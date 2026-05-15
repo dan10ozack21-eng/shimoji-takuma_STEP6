be67f813bd893ba2af020a8666788ce4d4b98102
dan10ozack21-eng <dan10ozack21@gmail.com>
2026-05-15 23:15:35 +0900

    設問5.6完了

---
+++ send.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="UTF-8">
+        <title>お問い合わせフォーム - 送信完了画面</title>
+    </head>
+    <body>
+        <h1>お問い合わせフォーム - 送信完了画面</h1>
+
+
+        <a href="contact.php">お問い合わせフォームに戻る</a>
+    </body>
+</html>
