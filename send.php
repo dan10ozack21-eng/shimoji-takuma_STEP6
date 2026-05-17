ed289b92640182628c51142150d42439f48e7f57
dan10ozack21-eng <dan10ozack21@gmail.com>
2026-05-17 22:29:24 +0900

    バックエンド処理 設問２(send.php)完了

--- send.php
+++ send.php
@@ -4,6 +4,22 @@ if ($_SERVER["REQUEST_METHOD"] !== "POST") {
     exit;
 }
 
+$name = htmlspecialchars($_POST['name'], ENT_QUOTES, 'UTF-8');
+$companyName = htmlspecialchars($_POST['companyName'], ENT_QUOTES, 'UTF-8');
+$email = htmlspecialchars($_POST['email'], ENT_QUOTES, 'UTF-8');
+$age = htmlspecialchars($_POST['age'], ENT_QUOTES, 'UTF-8');
+$message = htmlspecialchars($_POST['message'], ENT_QUOTES, 'UTF-8');
+
+$to = "sample@tng.com";
+$subject = "お問い合わせがありました。";
+$body = "名前: {$name}\n会社名:{$companyName}\n年齢: {$age}\n内容: {$message}";
+$headers = "From: {$email}";
+
+if (mb_send_mail($to, $subject, $body, $headers)) {
+    $result_message = "お問い合わせが送信されました！ありがとうございます！";
+} else {
+    $result_message = "申し訳ございません。送信に失敗しました。時間をおいて再度お試しください。";
+}
 ?>
 
 <!DOCTYPE html>
@@ -13,9 +29,11 @@ if ($_SERVER["REQUEST_METHOD"] !== "POST") {
         <title>お問い合わせフォーム - 送信完了画面</title>
     </head>
     <body>
-        <h1>お問い合わせフォーム - 送信完了画面</h1>
+        <main>
+            <h1>お問い合わせフォーム - 送信完了画面</h1>
+            <p><?php echo $result_message; ?></p>
 
-
-        <a href="contact.php">お問い合わせフォームに戻る</a>
+            <a href="contact.php">お問い合わせフォームに戻る</a>
+        </main>
     </body>
 </html>
